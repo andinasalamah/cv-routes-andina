8950b63ac9336037dfec3a7d12ab73deb10ffdc6
andinasalamah <andina.salamah@gmail.com>
2020-10-17 12:36:51 +0700

    add certifications

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
 export { default as Home } from './home';
 export { default as Profile } from './profile';
-export { default as Certification } from './certification';
+export { default as Certifications } from './certifications';
 
