a1cd608a2f21dbea2bc958169782a20a222dbaf5
andinasalamah <andina.salamah@gmail.com>
2020-10-20 10:59:55 +0700

    add useState

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,41 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState} from 'react';
 
+const Home = () => {
+const [myNumber, setMyNumber] = useState(1);
 
-class Home extends Component {
-  render() {
-    return(
-        <div className="banner-text">
+  return(
+        <div className="button">
+          <button onClick={() => {setMyNumber(myNumber + 1);}}>Click here to count the visited</button>
+          <p>{myNumber}</p>
 
+       <div className="banner-text">
               <img
                     src="https://www.flaticon.com/svg/static/icons/svg/1498/1498485.svg"
                     alt="avatar"
                     className="avatar-img"
-                />
-                <h1>Andina Salamah</h1>
-          <p>ERP | HTML/CSS | Java | Python | C# </p>
+                  />
+                
+              <h1>Andina Salamah</h1>
+              <p>ERP | HTML/CSS | Java | Python | C# </p>
 
-        <div className="social-links">
-          {/* LinkedIn */}
-          <a href="https://www.linkedin.com/in/andinasalamah/" rel="linkedin" target="_blank">
-              <img
-                  src="https://www.flaticon.com/svg/static/icons/svg/145/145807.svg"/>
-          </a>
+              <div className="social-links">
+                {/* LinkedIn */}
+                  <a href="https://www.linkedin.com/in/andinasalamah/" rel="linkedin" target="_blank">
+                    <img
+                      src="https://www.flaticon.com/svg/static/icons/svg/145/145807.svg"/>
+                  </a>
           
-          {/* Instagram */}
-          <a href="https://www.instagram.com/andinasalamah/" rel="instagram" target="_blank">
-          <img
-              src="https://www.flaticon.com/svg/static/icons/svg/145/145805.svg"/>
-          </a>
           
-          {/* Github */}
-          <a href="https://github.com/andinasalamah/" rel="github" target="_blank">
-          <img
-              src="https://www.flaticon.com/svg/static/icons/svg/1051/1051275.svg"/>
-          </a>
+                {/* Instagram */}
+                  <a href="https://www.instagram.com/andinasalamah/" rel="instagram" target="_blank">
+                    <img
+                      src="https://www.flaticon.com/svg/static/icons/svg/145/145805.svg"/>
+                  </a>
+          
+                {/* Github */}
+                  <a href="https://github.com/andinasalamah/" rel="github" target="_blank">
+                    <img
+                      src="https://www.flaticon.com/svg/static/icons/svg/1051/1051275.svg"/>
+                  </a>
+            </div>
+          </div>
         </div>
-      </div>
-    )
-  }
-}
+      )
+    }
 
 export default Home;
